a0f97e113012f619926ab8e4f9feb062ae8271d8
Charlesss-0 <aragonaugusto588@gmail.com>
2024-09-02 19:55:34 -0600

    initial commit
    - solve two-sum problem

---
+++ 1-two-sum/index.ts
@@ -0,0 +1,36 @@
+// Brute-Force Approach
+// function twoSum(nums: number[], target: number): number[] {
+// 	for (let i = 0; i < nums.length; i++) {
+// 		for (let j = 0; j < i; j++) {
+// 			if (nums[i] + nums[j] === target) {
+// 				return [j, i]
+// 			}
+// 		}
+// 	}
+
+// 	return []
+// }
+
+// HashMap Approach
+function twoSum(nums: number[], target: number): number[] {
+	const hash = new Map()
+
+	for (let i = 0; i < nums.length; i++) {
+		const diff = target - nums[i]
+
+		if (hash.has(diff)) {
+			return [hash.get(diff), i]
+		}
+
+		hash.set(nums[i], i)
+	}
+
+	return []
+}
+
+const array: number[] = [2, 5, 8, 3, 6, 3, 5]
+const target: number = 10
+
+const sum: number[] = twoSum(array, target)
+
+console.log(sum)
