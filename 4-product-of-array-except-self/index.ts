7978539465b9fc0e9826285f04b5d9454d48cf38
Charlesss-0 <aragonaugusto588@gmail.com>
2024-09-03 19:08:52 -0600

    solve "product of array except self" problem

---
+++ 4-product-of-array-except-self/index.ts
@@ -0,0 +1,42 @@
+// Brute-Force solution
+// export function productExceptSelf(nums: number[]): number[] {
+// 	let result: number[] = []
+
+// 	for (let i = 0; i < nums.length; i++) {
+// 		let product = 1
+
+// 		for (let j = 0; j < nums.length; j++) {
+// 			if (i !== j) {
+// 				product = product * nums[j]
+// 			}
+// 		}
+
+// 		result[i] = product
+// 	}
+
+// 	return result
+// }
+
+// One Pass solution
+export function productExceptSelf(nums: number[]): number[] {
+	let result: number[] = [...Array(nums.length).fill(1)]
+
+	let prefix: number = 1
+	for (let i = 0; i < nums.length; i++) {
+		result[i] = prefix
+		prefix *= nums[i]
+	}
+
+	let suffix: number = 1
+	for (let i = nums.length - 1; i >= 0; i--) {
+		result[i] *= suffix
+		suffix *= nums[i]
+	}
+
+	return result
+}
+
+const nums = [1, 2, 3, 4]
+const result = productExceptSelf(nums)
+
+console.log(result)
